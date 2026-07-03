47322490af24ae9ad27f81260cc6c9fea45b031d
patangesomnath5-svg <patangesomnath5@gmail.com>
2026-07-03 13:00:25 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,8 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
-
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../'))); // serves files from the root project folder
 // ---------- MongoDB Connection ----------
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB connected'))
